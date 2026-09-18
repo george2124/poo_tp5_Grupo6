5959bd649720dc7a7c67057a2143c663d308634f
Candela Kujarchik <candelaria.agostinaa@gmail.com>
2026-09-18 16:55:31 -0300

    Implementación de clases de soporte

--- target/classes/META-INF/maven/ar.edu.unju.fi.poo/poo-tp5-grupo6/src/main/java/ar/edu/unju/fi/poo/pto2/model/Paquete.java
+++ target/classes/META-INF/maven/ar.edu.unju.fi.poo/poo-tp5-grupo6/src/main/java/ar/edu/unju/fi/poo/pto2/model/Paquete.java
@@ -2,4 +2,54 @@ package ar.edu.unju.fi.poo.pto2.model;
 
 public class Paquete {
 
+    private String codigo;
+    private String descripcion;
+    private double peso;
+    private double volumen;
+
+    public Paquete(String codigo, String descripcion, double peso, double volumen) {
+        super();
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.peso = peso;
+        this.volumen = volumen;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete [codigo=" + codigo + ", descripcion=" + descripcion + ", peso=" + peso + ", volumen="
+                + volumen + "]";
+    }
 }
--- target/classes/META-INF/maven/ar.edu.unju.fi.poo/poo-tp5-grupo6/src/main/java/ar/edu/unju/fi/poo/pto2/model/Vehiculo.java
+++ target/classes/META-INF/maven/ar.edu.unju.fi.poo/poo-tp5-grupo6/src/main/java/ar/edu/unju/fi/poo/pto2/model/Vehiculo.java
@@ -2,4 +2,44 @@ package ar.edu.unju.fi.poo.pto2.model;
 
 public class Vehiculo {
 
+    private String patente;
+    private double capacidadPeso;
+    private double capacidadVolumen;
+
+    public Vehiculo(String patente, double capacidadPeso, double capacidadVolumen) {
+        super();
+        this.patente = patente;
+        this.capacidadPeso = capacidadPeso;
+        this.capacidadVolumen = capacidadVolumen;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public double getCapacidadPeso() {
+        return capacidadPeso;
+    }
+
+    public void setCapacidadPeso(double capacidadPeso) {
+        this.capacidadPeso = capacidadPeso;
+    }
+
+    public double getCapacidadVolumen() {
+        return capacidadVolumen;
+    }
+
+    public void setCapacidadVolumen(double capacidadVolumen) {
+        this.capacidadVolumen = capacidadVolumen;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo [patente=" + patente + ", capacidadPeso=" + capacidadPeso + ", capacidadVolumen="
+                + capacidadVolumen + "]";
+    }
 }
